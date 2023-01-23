e24ff8367405f23d8ce6efec95028b0bfb497312
aguiarHugo <aguiarhugofx@gmail.com>
2023-01-23 12:40:54 -0300

    first-commit

---
+++ script.js
@@ -0,0 +1,20 @@
+
+let firstNumber = prompt('Vamos realizar algumas operações matemáticas, digite o primeiro número: ')
+let secondNumber = prompt ('Digite o segundo número: ')
+
+firstNumber = Number(firstNumber)
+secondNumber = Number(secondNumber)
+
+const sum = firstNumber + secondNumber
+const sub = firstNumber - secondNumber
+const multi = firstNumber * secondNumber
+const div = firstNumber / secondNumber
+const restDiv = firstNumber % secondNumber
+const evenOdd = restDiv % 2 == 0? 'Par' : 'Impar'
+
+alert('Soma: ' + sum)
+alert('Subtração: ' + sub)
+alert('Mutiplicação: ' + multi)
+alert('Divisão: ' + div)
+alert('Resto da divisão: ' + restDiv)
+alert ('O número é: ' + evenOdd)
